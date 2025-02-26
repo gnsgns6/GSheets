27360c4add32637407c954842f5eb43be86654d2
Gopi Nath <gnsgns199@gmail.com>
2025-02-26 23:33:21 +0530

    Add FormulaBar component with cell reference and input

--- src/components/FormulaBar.jsx
+++ src/components/FormulaBar.jsx
@@ -1,1 +1,18 @@
- 
+import React from 'react';
+
+function FormulaBar({ activeCell }) {
+  return (
+    <div className="formula-bar">
+      <div className="cell-reference">
+        {activeCell || ''}
+      </div>
+      <input 
+        type="text" 
+        className="formula-input"
+        placeholder="Enter formula or value"
+      />
+    </div>
+  );
+}
+
+export default FormulaBar;
