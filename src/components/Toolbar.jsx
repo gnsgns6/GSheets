0c8a9ac53ef2868a9bf9073d59765674952c57d5
Gopi Nath <gnsgns199@gmail.com>
2025-02-28 02:44:37 +0530

    Refactor Toolbar: Organize controls into logical groups and enhance UI with icons and tooltips

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -3,62 +3,68 @@ import React from 'react';
 function Toolbar() {
   return (
     <div className="toolbar">
-      <select className="font-family">
-        <option>Arial</option>
-        <option>Google Sans</option>
-        <option>Roboto</option>
-        <option>Roboto Mono</option>
-      </select>
-      
-      <select className="font-size">
-        {[8, 9, 10, 11, 12, 14, 16, 18, 24, 36].map(size => (
-          <option key={size}>{size}</option>
-        ))}
-      </select>
+      {/* File Operations Group */}
+      <div className="toolbar-group">
+        <button title="New"><i className="material-icons">add</i></button>
+        <button title="Open"><i className="material-icons">folder_open</i></button>
+        <button title="Save"><i className="material-icons">save</i></button>
+        <button title="Download"><i className="material-icons">download</i></button>
+      </div>
 
-      <div className="separator" style={{ width: '1px', height: '20px', backgroundColor: '#e1e1e1', margin: '0 4px' }} />
-      
-      <button title="Bold (⌘B)">
-        <i className="material-icons">format_bold</i>
-      </button>
-      <button title="Italic (⌘I)">
-        <i className="material-icons">format_italic</i>
-      </button>
-      <button title="Underline (⌘U)">
-        <i className="material-icons">format_underlined</i>
-      </button>
-      
-      <div className="separator" style={{ width: '1px', height: '20px', backgroundColor: '#e1e1e1', margin: '0 4px' }} />
-      
-      <button title="Text color">
-        <i className="material-icons">format_color_text</i>
-      </button>
-      <button title="Fill color">
-        <i className="material-icons">format_color_fill</i>
-      </button>
-      
-      <div className="separator" style={{ width: '1px', height: '20px', backgroundColor: '#e1e1e1', margin: '0 4px' }} />
-      
-      <button title="Align left">
-        <i className="material-icons">format_align_left</i>
-      </button>
-      <button title="Align center">
-        <i className="material-icons">format_align_center</i>
-      </button>
-      <button title="Align right">
-        <i className="material-icons">format_align_right</i>
-      </button>
-      
-      <div className="separator" style={{ width: '1px', height: '20px', backgroundColor: '#e1e1e1', margin: '0 4px' }} />
-      
-      <select title="Functions">
-        <option value="">Functions</option>
-        <option value="SUM">SUM</option>
-        <option value="AVERAGE">AVERAGE</option>
-        <option value="MAX">MAX</option>
-        <option value="MIN">MIN</option>
-        <option value="COUNT">COUNT</option>
-      </select>
+      {/* Edit Operations Group */}
+      <div className="toolbar-group">
+        <button title="Undo"><i className="material-icons">undo</i></button>
+        <button title="Redo"><i className="material-icons">redo</i></button>
+        <button title="Copy"><i className="material-icons">content_copy</i></button>
+        <button title="Cut"><i className="material-icons">content_cut</i></button>
+        <button title="Paste"><i className="material-icons">content_paste</i></button>
+      </div>
+
+      {/* Formatting Group */}
+      <div className="toolbar-group">
+        <select className="font-family" title="Font">
+          <option>Arial</option>
+          <option>Google Sans</option>
+          <option>Roboto</option>
+        </select>
+        <select className="font-size" title="Font size">
+          {[8, 9, 10, 11, 12, 14, 16, 18, 20, 24, 28, 32].map(size => (
+            <option key={size}>{size}</option>
+          ))}
+        </select>
+      </div>
+
+      {/* Text Formatting */}
+      <div className="toolbar-group">
+        <button title="Bold"><i className="material-icons">format_bold</i></button>
+        <button title="Italic"><i className="material-icons">format_italic</i></button>
+        <button title="Underline"><i className="material-icons">format_underlined</i></button>
+        <button title="Text Color"><i className="material-icons">format_color_text</i></button>
+        <button title="Fill Color"><i className="material-icons">format_color_fill</i></button>
+      </div>
+
+      {/* Alignment Group */}
+      <div className="toolbar-group">
+        <button title="Align Left"><i className="material-icons">format_align_left</i></button>
+        <button title="Align Center"><i className="material-icons">format_align_center</i></button>
+        <button title="Align Right"><i className="material-icons">format_align_right</i></button>
+      </div>
+
+      {/* Data Operations */}
+      <div className="toolbar-group">
+        <button title="Sort"><i className="material-icons">sort</i></button>
+        <button title="Filter"><i className="material-icons">filter_list</i></button>
+        <button title="Chart"><i className="material-icons">insert_chart</i></button>
+        <button title="Functions"><i className="material-icons">functions</i></button>
+      </div>
+
+      {/* Insert Options */}
+      <div className="toolbar-group">
+        <button title="Insert Row"><i className="material-icons">add_box</i></button>
+        <button title="Insert Column"><i className="material-icons">view_column</i></button>
+        <button title="Insert Comment"><i className="material-icons">comment</i></button>
+        <button title="Insert Image"><i className="material-icons">image</i></button>
+      </div>
     </div>
   );
 }
